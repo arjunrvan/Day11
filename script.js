6deb7b3f062f8b0f55f9c4668b90d27946633b8e
arjunrvan <arjunrvan@gmail.com>
2021-09-28 20:11:45 +0800

    update color gradient

--- script.js
+++ script.js
@@ -4,51 +4,62 @@ window.addEventListener("keyup", keyRelease);
 
 var keyHolder = document.getElementById('key-holder');
 
+var container = document.getElementById('container');
+
 var keyList = [
     {
         keyName: "A",
         src: new Howl({src:['audio/1.mp3']}),
-        keyCode: 65
+        keyCode: 65,
+        bgColor: 'linear-gradient(to right,red,yellow)'
     },
     {
         keyName: "S",
         src: new Howl({src:['audio/2.mp3']}),
-        keyCode: 83
+        keyCode: 83,
+        bgColor: 'linear-gradient(to right,yellow,green)'
     },
     {
         keyName: "D",
         src: new Howl({src:['audio/3.mp3']}),
-        keyCode: 68
+        keyCode: 68,
+        bgColor: 'linear-gradient(to right,green,violet)'
     },
     {
         keyName: "F",
         src: new Howl({src:['audio/4.mp3']}),
-        keyCode: 70
+        keyCode: 70,
+        bgColor: 'linear-gradient(to right,violet,pink)'
     },
     {
         keyName: "G",
         src: new Howl({src:['audio/5.mp3']}),
-        keyCode: 71
+        keyCode: 71,
+        bgColor: 'linear-gradient(to right,pink,orange)'
     },
     {
         keyName: "H",
         src: new Howl({src:['audio/6.mp3']}),
-        keyCode: 72
+        keyCode: 72,
+        bgColor: 'linear-gradient(to right,orange,green)'
     },
     {
         keyName: "J",
         src: new Howl({src:['audio/7.mp3']}),
-        keyCode: 74
+        keyCode: 74,
+        bgColor: 'linear-gradient(to right,green,magenta)'
     },
     {
         keyName: "K",
         src: new Howl({src:['audio/8.mp3']}),
-        keyCode: 75
+        keyCode: 75,
+        bgColor: 'linear-gradient(to right,magenta,red)'
     },
     {
         keyName: "L",
         src: new Howl({src:['audio/9.mp3']}),
-        keyCode: 76
+        keyCode: 76,
+        bgColor: 'linear-gradient(to right,red,black)'
     },
 ]
 
@@ -68,10 +79,9 @@ for (i = 0; i < keyList.length; i++) {
     whiteTile.style.height = '200px';
     whiteTile.style.lineHeight = '200px';
     whiteTile.style.borderRadius = '20px';
-    whiteTile.style.boxShadow = '2px 2px 2px 2px rgba(255,255,0,0.5)';
+    whiteTile.style.boxShadow = '3px 3px 3px rgba(255,255,255,0.5)';
 
     keyHolder.appendChild(whiteTile);
-
    
 }
 
@@ -93,6 +103,9 @@ function keyPressed (evt) {
             
     keyId[pos].classList.add("animate");
     keyId[pos].classList.remove("animateOut");
+
+    keyId[pos].style.backgroundImage = keyList[pos].bgColor;
+    setTimeout(function() {keyId[pos].style.backgroundImage = 'none';},1000);
 }
 
 function keyRelease (evt) {
@@ -102,5 +115,7 @@ function keyRelease (evt) {
 
     keyId[pos].classList.add("animateOut");
     keyId[pos].classList.remove("animate");
+
+    // keyId[pos].style.backgroundImage = 'none';
 }   
 
